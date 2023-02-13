ec008fffb16fe4a2c7de4600cc53f1fb0d17a7cd
Juvi09 <ntuthuko.s.ndlovu@gmail.com>
2023-02-13 15:55:26 +0200

    form component and validation

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// -- GLOBAL --
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
+const formEl = document.querySelector('.form');
 
+// -- COUNTER COMPONENT ---
 const inputHandler = () => {
     //Determine maximum number of characters.
     const maxNrChars = 150;
@@ -17,3 +20,30 @@ const inputHandler = () => {
 }  
 
 textareaEl.addEventListener('input', inputHandler);
+
+// -- FORM COMPONENT --
+const submitHandler = event => {
+    // prevent default broswer action (submitting from data to 'action' -address and refreshing page).
+    event.preventDefault();
+
+   // Get text from textarea
+   const text = textareaEl.value;
+   //console.log(text);
+
+   //Validate text (e.g check if #hashtag is present and text is long enough)
+   if (text.includes('#')){
+      formEl.classList.add('form--valid');
+
+      setTimeout(() => {
+          formEl.classList.remove('form--valid');
+      }, 2000);
+   } else {
+      formEl.classList.add('form--invalid');
+
+      setTimeout(() => {
+        formEl.classList.remove('form--invalid');
+    }, 2000);
+   }
+};
+
+formEl.addEventListener('submit', submitHandler);
